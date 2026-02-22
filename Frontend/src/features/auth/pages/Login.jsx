2b6e6fb68793965f94b404805e345162c3aee189
AshishBiswas-2027 <ab23ceb0b07@student.nitw.ac.in>
2026-02-22 16:45:06 +0530

    feat: initialize frontend with React, Vite, and routing
    - Added package.json for project dependencies and scripts
    - Included Vite logo SVG in public directory
    - Created main App component and routing setup
    - Implemented Login and Register pages with form handling
    - Added styles for authentication forms
    - Set up main entry point for React application
    - Configured Vite for development and build processes

---
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -0,0 +1,36 @@
+import React,{useState} from 'react'
+import "../styles/form.scss"
+import { Link } from 'react-router'
+import axios from 'axios'
+
+const [username, setUsername] = useState("")
+const [password,setPassword] = useState("")
+
+function handleFormSubmit(e) {
+    e.preventDefault()
+
+    axios.post("http://localhost:3000/api/auth/login", {
+        username,
+        password
+    }, {
+        withCredentials:true
+    })
+}
+
+const Login = () => {
+  return (
+      <main>
+          <div className="form-container">
+              <h1>Login</h1>
+              <form onSubmit={handleFormSubmit} >
+                  <input onInput={(e) => { setUsername(e.target.value) }}  type="text" name='username' placeholder='Enter username' />
+                  <input onInput={(e)=>{setPassword(e.target.value)}} type="password" name="password" placeholder='Enter password' />
+                  <button type='submit'>Login</button>
+              </form>
+              <p>Don't have an account?<Link className="toggle-auth-form" to="/register"> Register here</Link></p>
+          </div>
+    </main>
+  )
+}
+
+export default Login
